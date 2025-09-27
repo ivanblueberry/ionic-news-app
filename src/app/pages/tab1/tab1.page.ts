12cb4a247174052f543b8fb803d66dfa4ed5b1a4
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-27 10:30:58 -0600

    feat(tab1): update layout and display news articles in tab1 page

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonCol, IonRow, IonCard, IonCardSubtitle, IonCardTitle, IonImg, IonCardContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
 import { News } from 'src/app/services/news';
-import { NewsResponse } from 'src/app/interfaces';
+import { Article, NewsResponse } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonCardContent, IonImg, IonCardTitle, IonCardSubtitle, IonCard, IonRow, IonCol, IonGrid, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page implements OnInit {
+
+  public articles: Article[] = [];
+
   constructor( private newService: News ) { }
 
   ngOnInit() {
     this.newService.getTopHeadlines()
-      .subscribe( articles => {
-        console.log( articles );
-      });
+      .subscribe( articles => this.articles.push( ...articles ));
   }
 }
