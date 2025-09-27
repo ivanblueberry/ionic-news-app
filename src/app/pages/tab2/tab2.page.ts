4d02e5ade8e59b0ae8647262c4503562fdccb086
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-27 12:03:18 -0600

    feat(tab2): add segment control for category selection and update headers

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonLabel,
+            IonSegmentButton,
+            IonSegment,
+            IonHeader,
+            IonToolbar,
+            IonTitle,
+            IonContent]
 })
 export class Tab2Page {
 
+  public categories: string[] = ['business',
+                                 'entertainment',
+                                 'general',
+                                 'health',
+                                 'science',
+                                 'sports',
+                                 'technology'];
+  /**
+   * Holds the currently selected category. By default, it is set
+   * to the first element in the categories array.
+   */
+  public selectedCategory: string = this.categories[0];
+
   constructor() {}
+
+  segmentChanged( category: any ) {
+    console.log(category);
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,19 @@ import { AppComponent } from './app/app.component';
 // To make API requests
 import { provideHttpClient } from '@angular/common/http';
 
+// Iconography Setup
+import { addIcons } from 'ionicons';
+import { globeOutline,
+         personOutline,
+         starOutline,
+       } from 'ionicons/icons'
+
+addIcons({
+  'person-outline': personOutline,
+  'globe-outline' : globeOutline,
+  'star-outline'  : starOutline
+})
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
