28d7fb36e4765c836a8ecb29cf30b42cbca2ec34
Ivan Torres <francisco.ivantf@hotmail.com>
2025-09-27 15:19:46 -0600

    feat(environment): add API key and URL to environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  // IMPORTANT: Replace 'YOUR_API_KEY_HERE' with your actual API key for the service.
+  apiKey: 'YOUR_API_KEY_HERE', // Fake API Key: 0ff123456abc7890def123456abc7890def1
+  apiUrl: 'https://newsapi.org/v2'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // IMPORTANT: Replace 'YOUR_API_KEY_HERE' with your actual API key for the service.
+  apiKey: 'YOUR_API_KEY_HERE', // Fake API Key: 0ff123456abc7890def123456abc7890def1
+  apiUrl: 'https://newsapi.org/v2'
 };
 
 /*
