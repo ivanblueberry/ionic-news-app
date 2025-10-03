b6e77663e8c89c96c6bec01e11d5fea5467070c5
Axel Tapia <axurmen214@gmail.com>
2025-10-02 19:40:22 -0600

    feat: Implement favorites feature with local storage

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonIcon} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
+
+import { StorageService } from '../../services/storage';
+import { ArticlesComponent } from 'src/app/components/articles/articles.component';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, ArticlesComponent,IonGrid, IonRow, IonCol, IonIcon],
 })
 export class Tab3Page {
-  constructor() {}
+  constructor( public storageService: StorageService ) {}
 }
---
+++ src/app/services/storage.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { Article } from '../interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  private _storage: Storage | null = null;
+  private _localArticles: Article[] = [];
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  get getLocalArticles() {
+    return [...this._localArticles];
+  }
+
+  async init() {
+    // Initilizate DB
+    const storage = await this.storage.create();
+    this._storage = storage;
+    // Load the Articles Save
+    this.loadFavorites();
+  }
+
+  async saveRemoveArticle(article: Article) {
+
+
+    const exists = this._localArticles.find(localArticle => localArticle.title === article.title);
+
+    if (exists) {
+      this._localArticles = this._localArticles.filter(localArticle => localArticle.title !== article.title);
+    } else {
+      this._localArticles = [article, ...this._localArticles];
+    }
+
+    this._storage?.set('articles', this._localArticles);
+  }
+
+  async loadFavorites() {
+    try {
+      const articles = await this._storage?.get('articles');
+      this._localArticles = articles || [];
+    } catch (error) {
+      console.error('Error loading favorites', error);
+      this._localArticles = [];
+    }
+  }
+}
